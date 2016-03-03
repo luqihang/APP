d39e1528cbd9b9b2c22250ae7fb48357ea38ab61
luqihang <599131801@qq.com>
2016-03-03 22:53:48 +0800

    总结
    测试提交

---
+++ containers/App.js
@@ -0,0 +1,80 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { pushState } from 'redux-router'
+import Explore from '../components/Explore'
+import { resetErrorMessage } from '../actions'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDismissClick = this.handleDismissClick.bind(this)
+  }
+
+  handleDismissClick(e) {
+    this.props.resetErrorMessage()
+    e.preventDefault()
+  }
+
+  handleChange(nextValue) {
+    this.props.pushState(null, `/${nextValue}`)
+  }
+
+  renderErrorMessage() {
+    const { errorMessage } = this.props
+    if (!errorMessage) {
+      return null
+    }
+
+    return (
+      <p style={{ backgroundColor: '#e99', padding: 10 }}>
+        <b>{errorMessage}</b>
+        {' '}
+        (<a href="#"
+            onClick={this.handleDismissClick}>
+          Dismiss
+        </a>)
+      </p>
+    )
+  }
+
+  render() {
+    const { children, inputValue } = this.props
+    return (
+      <div>
+        <Header />
+        <Explore value={inputValue}
+                 onChange={this.handleChange} />
+        <hr />
+        {this.renderErrorMessage()}
+        {children}
+        <Footer />
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  // Injected by React Redux
+  errorMessage: PropTypes.string,
+  resetErrorMessage: PropTypes.func.isRequired,
+  pushState: PropTypes.func.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  // Injected by React Router
+  children: PropTypes.node
+}
+
+function mapStateToProps(state) {
+  return {
+    errorMessage: state.errorMessage,
+    inputValue: state.router.location.pathname.substring(1)
+  }
+}
+
+export default connect(mapStateToProps, {
+  resetErrorMessage,
+  pushState
+})(App)
