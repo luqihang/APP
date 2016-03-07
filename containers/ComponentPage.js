082036644a6e3d49db11b46ec410d14ea76fc5fc
luqihang <599131801@qq.com>
2016-03-07 21:42:29 +0800

    组件页

---
+++ containers/ComponentPage.js
@@ -0,0 +1,29 @@
+
+import React from 'React'
+
+import { connect } from 'react-redux'
+
+class ComponentPage extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+
+}
